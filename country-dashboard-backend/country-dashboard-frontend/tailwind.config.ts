ed11b89344ca39fd7a95fd80fa7bdb44e336ce17
satya ranjan <satya.ranjan.robomq@gmail.com>
2024-12-05 21:46:55 +0530

    change the congig of the tailwind add module.export

--- country-dashboard-backend/country-dashboard-frontend/tailwind.config.ts
+++ country-dashboard-backend/country-dashboard-frontend/tailwind.config.ts
@@ -16,3 +16,9 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: { extend: {} },
+  plugins: [],
+};
